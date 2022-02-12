73dd727b499e798200229e7b39804af17b361c3a
Almog Lev <almoglev3@gmail.com>
2022-02-12 16:31:58 +0200

    Flipping cards

--- src/App.js
+++ src/App.js
@@ -35,7 +35,7 @@ function App() {
         }) 
       }
 
-      resetTurn()
+      setTimeout(() => resetTurn(), 1000)
     }
   }, [choiceOne, choiceTwo])
 
@@ -69,7 +69,11 @@ function App() {
 
       <div className='card-grid'>
         {cards.map(card => (
-          <SingleCard key={card.id} card={card} handleChoice={handleChoice} />
+          <SingleCard
+           key={card.id}
+           card={card} 
+           handleChoice={handleChoice} 
+           flipped={card === choiceOne || card == choiceTwo || card.matched === true}/>
         ))}  
       </div> 
     </div>
