c75e3d245feb84e0601166600bb45e0bfe7d4372
Almog Lev <almoglev3@gmail.com>
2022-02-12 15:48:17 +0200

    Add HandleChoice

--- src/components/SingleCard.js
+++ src/components/SingleCard.js
@@ -1,13 +1,17 @@
 import './SingleCard.css';
 
 
-function SingleCard({ card }) {
+function SingleCard({ card, handleChoice}) {
+
+    const handleClick = () => {
+        handleChoice(card)
+    }
 
   return (
     <div className="card">
     <div>
       <img src={card.src} className="front" alt="card front" />
-      <img src="/img/cover.png" className="back" alt="card back" />
+      <img src="/img/cover.png" className="back" alt="card back" onClick={handleClick} />
     </div>
   </div>
   )
