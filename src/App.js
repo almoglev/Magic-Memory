9b3d31254ad32eddaf01fb0ee9c1aa256da07569
Almog Lev <almoglev3@gmail.com>
2022-02-18 00:10:02 +0200

    Fix little bug

--- src/App.js
+++ src/App.js
@@ -80,7 +80,7 @@ function App() {
            key={card.id}
            card={card} 
            handleChoice={handleChoice} 
-           flipped={card === choiceOne || card == choiceTwo || card.matched === true}
+           flipped={card === choiceOne || card === choiceTwo || card.matched === true}
            disabled= {disabled} 
           />
         ))}  
